5726a4923b67355f3e45e5e54e49aac4e12302c0
Deivyson J. Silva <139878479+deivysonjds@users.noreply.github.com>
2025-09-13 15:20:24 +0000

    added: tests saldoinicial

--- src/test/java/SaldoInicial.java
+++ src/test/java/SaldoInicial.java
@@ -2,15 +2,24 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import com.example.DigitalWallet;
 
 class SaldoInicial {
-        void deveConfigurarSaldoInicialCorreto() {
-            
+        @ParameterizedTest
+        @ValueSource(doubles={10,0,20})
+        void deveConfigurarSaldoInicialCorreto(double balance) {
+            DigitalWallet digitalWallet = new DigitalWallet("Deivyson", balance);
+            assertEquals(balance, digitalWallet.getBalance());
         }
 
-        void deveLancarExcecaoParaSaldoInicialNegativo() {
-            
+        @ParameterizedTest
+        @ValueSource(doubles={-10,-5,-20})
+        void deveLancarExcecaoParaSaldoInicialNegativo(double balance) {
+            assertThrows(IllegalArgumentException.class, ()->{
+                DigitalWallet digitalWallet = new DigitalWallet("Deivyson", balance);
+            });
         }
     }
