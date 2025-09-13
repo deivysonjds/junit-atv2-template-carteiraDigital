2c74a6a8598c9613f15f8527e36f3588a1b0afd7
Deivyson J. Silva <139878479+deivysonjds@users.noreply.github.com>
2025-09-13 15:13:04 +0000

    added: tests deposito

--- src/test/java/Deposito.java
+++ src/test/java/Deposito.java
@@ -1,6 +1,8 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -8,13 +10,27 @@ import com.example.DigitalWallet;
 
 
 class Deposito {
-        
-        void deveDepositarValoresValidos(double amount) {
-            
+
+        DigitalWallet digitalWallet;
+        @BeforeEach
+        void instanceClass(){
+            digitalWallet = new DigitalWallet("Deivyson", 0);
         }
 
-        
+        @ParameterizedTest
+        @ValueSource(doubles = {10.0, 0.01, 999.99})
+        void deveDepositarValoresValidos(double amount) {
+            double beforeValue = digitalWallet.getBalance();
+            digitalWallet.deposit(amount);
+
+            assertEquals(amount + beforeValue, digitalWallet.getBalance());
+        }
+
+        @ParameterizedTest
+        @ValueSource(doubles = {-10.0, -0.01, -999.99})
         void deveLancarExcecaoParaDepositoInvalido(double amount) {
-            
+            assertThrows(IllegalArgumentException.class, ()->{
+                digitalWallet.deposit(amount);
+            });
         }
     }
